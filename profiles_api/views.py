15c2b3fc5099c153384a1f637230bd48f6b01862
jevon wirjono <kurisaq@gmail.com>
2021-11-01 11:19:48 +0700

    finish training development

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status,viewsets
 
 from profiles_api import serializers
 
@@ -41,5 +41,46 @@ class HelloApiView(APIView):
     def delete(self,request,pk=None):
         """Delete object"""
         return Response({'method':'DELETE'})
+        # status=400 juga bisa
 
-            # status=400 juga bisa
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+    serializer_class = serializers.HelloSerializer
+
+    def list(self,request):
+        """Return hello message"""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self,request):
+        """Create a new hello message"""
+        Tserializer = self.serializer_class(data=request.data)
+
+        if Tserializer.is_valid():
+            name = Tserializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'msg':message})
+        else:
+            return Response(Tserializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request, pk=None):
+        """Handle getting an object by its ID"""
+        return Response({'http_method':'GET'})
+
+    def update(self,request,pk=None):
+        """Handle updating object"""
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        """Handle updating  part of an object"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        """Handle removing an object"""
+        return Response({'http_method':'DELETE'})
